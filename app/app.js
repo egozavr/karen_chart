261a80efa3161588c2791687f579a7a2cbb2fc37
Egozavr <yagerr@gmail.com>
2017-04-06 06:47:27 +0300

    add x axis (tick labels need tuninig)

--- app/app.js
+++ app/app.js
@@ -33,7 +33,6 @@ function ChartController($http, moment, TS_FORMAT) {
         self.data = [];
         for (let i = 0; i < data.length; i++) {
             let job = data[i];
-
             self.data.push({
                 name: job.jobname,
                 start: job.runstarttimestamp,
@@ -51,16 +50,23 @@ function ChartController($http, moment, TS_FORMAT) {
 
     function plotChart() {
         let jobData = self.data;
-        let margin = {top: 20, right: 0, bottom: 40, left: 50};
+        let margin = {top: 20, right: 0, bottom: 100, left: 50};
         let width = 800 - margin.left - margin.right;
         let height = 600 - margin.top - margin.bottom;
+
+        let xsc = d3.scale.ordinal()
+            .domain(jobData.map(function (d) {return d.start}))
+            .rangeRoundBands([0, width], .15);
 
         let ysc = d3.scale.linear()
             .domain([0, d3.max(jobData, function (d) {
                 return d.fact
             })])
             .range([height, 0]);
-        // let xsc = d3.scale.ordinal().domain().rangeRoundBands([0, width], .15);
+
+        let xAxis = d3.svg.axis()
+            .scale(xsc).orient('bottom');
+            // .tickValues(function (d) {return d});
 
         let chart = d3.select('.job-chart')
             .attr('width', width + margin.left + margin.right)
@@ -68,22 +74,27 @@ function ChartController($http, moment, TS_FORMAT) {
             .append('g').attr('class', 'inner-chart')
             .attr('transform', `translate(${margin.left},${margin.top})`);
 
-        let barWidth = (width / jobData.length);
+        chart.append('g')
+            .attr('class', 'job-chart-xaxis')
+            .attr('transform', `translate(0, ${height})`)
+            .call(xAxis)
+            .selectAll('text')
+            .attr({
+                x: 9, y: 0, dy: '.35em'
+            })
+            .attr('transform', 'rotate(90)')
+        ;
 
         let bar = chart.selectAll('.bar')
             .data(jobData)
             .enter().append('rect')
             .attr('class', 'bar')
-            .attr('x', function (d, i) {
-                return i * barWidth
-            })
-            .attr('y', function (d) {
-                return ysc(d.fact)
-            })
+            .attr('x', function (d) {return xsc(d.start)})
+            .attr('y', function (d) {return ysc(d.fact)})
             .attr('height', function (d) {
                 return height - ysc(d.fact)
             })
-            .attr('width', barWidth - 3);
+            .attr('width', xsc.rangeBand());
     }
 
 }
